9a5a0e88edb18abdcf340713e8c49d411ad95dbf
Yusuf Olokoba <olokobayusuf@gmail.com>
2021-05-27 17:40:12 -0400

    Upgrade ML-Pose dependency

--- Assets/PoseCam.cs
+++ Assets/PoseCam.cs
@@ -8,20 +8,18 @@ namespace NatSuite.Examples {
     using UnityEngine;
     using NatSuite.Devices;
     using NatSuite.ML;
+    using NatSuite.ML.Features;
     using NatSuite.ML.Vision;
     using NatSuite.ML.Visualizers;
     using NatSuite.ML.Extensions;
 
     public class PoseCam : MonoBehaviour {
 
-        [Header("ML")]
-        public MLModelData modelData;
-
-        [Header("UI")]
         public MLBodyPoseVisualizer visualizer;
 
+        private MLModelData modelData;
         private MLModel model;
-        private MLAsyncPredictor<MLBodyPose> predictor;
+        private MLAsyncPredictor<MoveNetPredictor.Pose> predictor;
         private CameraDevice cameraDevice;
         private Texture2D preview;
 
@@ -31,28 +29,36 @@ namespace NatSuite.Examples {
                 Debug.LogError(@"User did not grant camera permissions");
                 return;
             }
-            // Create predictor
-            model = modelData.Deserialize();
-            predictor = new MoveNetPredictor(model).ToAsync();
             // Get the default camera device
             var query = new MediaDeviceQuery(MediaDeviceCriteria.CameraDevice);
             cameraDevice = query.current as CameraDevice;
             // Start the camera preview
             cameraDevice.previewResolution = (1280, 720);
             preview = await cameraDevice.StartRunning();
+            Debug.Log("Started camera preview");
             // Display preview
             visualizer.Render(preview, null);
+            // Create predictor
+            modelData = await MLModelData.FromHub("@natsuite/movenet-lightning");
+            model = modelData.Deserialize();
+            predictor = new MoveNetPredictor(model).ToAsync();
+            Debug.Log("Created predictor");
         }
 
         async void Update () {
             // Check that the camera is running
             if (!preview)
                 return;
+            // Check that the predictor has been loaded
+            if (predictor == null)
+                return;
             // Check that the predictor is ready for more predictions
             if (!predictor.readyForPrediction)
                 return;
             // Detect
-            var pose = await predictor.Predict(preview);
+            var input = new MLImageFeature(preview);
+            (input.mean, input.std) = modelData.normalization;
+            var pose = await predictor.Predict(input);
             // Visualize
             visualizer.Render(preview, pose);
         }
@@ -63,6 +69,7 @@ namespace NatSuite.Examples {
                 cameraDevice.StopRunning();
             // Dispose model
             model?.Dispose();
+            predictor?.Dispose();
         }
     }
 }
